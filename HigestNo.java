628f239abe1a4844579da8e269463806ebb25e8f
Zeaan <59172904+Zeaan@users.noreply.github.com>
2020-11-01 13:12:21 +0530

    Update HigestNo.java
    I have added a separate else block if in case the user enters equal numbers. That will let the user know to enter different numbers. Also, I have used else if, which makes the code more efficient. The problem with if statements is that the compiler will go to the next if statement even though the earlier if the statement was satisfied. When we use else if, the compiler comes of the conditional statements when it finds that the statement which fulfills the condition. In your case, if num1 is greater, then the compiler will go to 2nd and 3rd if statement also because you have used if statements. When we use else if, if num1 is greater, then the compiler won't check the remaining conditions. The use of if statement and else if statements will give the same output, however, else if is more efficient than if.

--- HigestNo.java
+++ HigestNo.java
@@ -11,14 +11,24 @@ public static void main(String[] args) {
 	int num2=s.nextInt();
 	int num3=s.nextInt();
 	
-	if(num1>num2 &&num1>num3){
-	System.out.println("num1 :"+num1+" is highest ");	
+	if(num1>num2){
+		if(num1>num3){
+			System.out.println("num1 :"+num1+" is highest ");
+		}
+		else{
+			System.out.println("num3 :"+num3+" is highest ");
+		}
+	else if{
+		if(num2>num3){
+			System.out.println("num2 :"+num2+" is highest ");
+		}
+		else{
+			System.out.println("num3 :"+num3+" is highest ");
+		}
 	}
-	if(num2>num3 &&num2>num1){
-		System.out.println("num2 :"+num2+" is highest ");	
-		}
-	if(num3>num2 &&num3>num1){
-		System.out.println("num3 :"+num3+" is highest ");	
-		}
+	else{
+		//This block helps if num1=num2 or num2=num3 or num1=num3 or num1=num2=num3
+		System.out.println("Please enter different numbers");
+	}
 }
 }
